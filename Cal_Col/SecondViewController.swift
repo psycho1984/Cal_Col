41af4b78e47633dd5c881a5396c65281739e5e8b
avi <aviledovsky@gmail.com>
2016-10-17 05:10:30 -0700

    Initial Commit

---
+++ Cal_Col/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Cal_Col
+//
+//  Created by aviledovsky on 17/10/2016.
+//  Copyright © 2016 aviledovsky. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
